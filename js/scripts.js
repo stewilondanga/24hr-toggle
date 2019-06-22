d277ead6079231617edfc47d3293f5d66ec60681
Stewart ilondanga <stilondanga@gmail.com>
2019-06-22 12:44:47 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-/*var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+var navigate = (function() {
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
 
-var checkBox = $('#check');
+/*var checkBox = $('#check');
 var sky = $('.sky');
 var stars = $('.stars-1, .stars-2, .stars-3, .stars-4')
 
