dcb3e782ff144e07446e2075e70940fd51593bfc
Stewart ilondanga <stilondanga@gmail.com>
2019-06-22 12:44:12 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,12 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+var checkBox = $('#check');
+var sky = $('.sky');
+var stars = $('.stars-1, .stars-2, .stars-3, .stars-4')
+
+checkBox.change(function(){
+    sky.toggleClass('sky__night');
+    stars.toggleClass('stars__night');
+});
